d4403b26157c58c689ae2e2703974479ecee424c
Tan Gemicioglu <tg399@cornell.edu>
2026-09-19 02:56:40 -0400

    fix(cask): remove deprecated verified parameter (#16)

--- Casks/wardian.rb
+++ Casks/wardian.rb
@@ -2,14 +2,12 @@ cask "wardian" do
   version "0.6.0"
 
   on_arm do
-    url "https://github.com/wardian-app/Wardian/releases/download/v#{version}/Wardian_#{version}_aarch64.dmg",
-        verified: "github.com/wardian-app/Wardian/"
+    url "https://github.com/wardian-app/Wardian/releases/download/v#{version}/Wardian_#{version}_aarch64.dmg"
     sha256 "7f15029c3be79d6641c98c5b9a621f10709d00a6f98a579ac3ae31f2ea43886e"
   end
 
   on_intel do
-    url "https://github.com/wardian-app/Wardian/releases/download/v#{version}/Wardian_#{version}_x64.dmg",
-        verified: "github.com/wardian-app/Wardian/"
+    url "https://github.com/wardian-app/Wardian/releases/download/v#{version}/Wardian_#{version}_x64.dmg"
     sha256 "2a0b5eca9439dc6d1ee50bd41a247d55be1e833ed9080f4c30db319a56d524c8"
   end
 
